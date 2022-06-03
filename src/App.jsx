1a54cba85fa560003ba8001eaebdc1de5131c58a
Tomas Garcia <tomasgarciadev@gmail.com>
2022-06-03 15:52:40 +0200

    About, Contact and Header components rendering

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,16 @@
 import React from 'react';
+import Header from './componets/header/Header'; 
+import About from './componets/about/About'
+import Contact from './componets/contact/Contact'
 
 function App() {
   return (
-    <div>App</div>
+    <>
+      <Header />
+      <About />
+      <Contact />
+
+    </>
   )
 }
 
