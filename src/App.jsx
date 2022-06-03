54f75c51f2112c75251a06ff5ac97817d0c1f25f
Tomas Garcia <tomasgarciadev@gmail.com>
2022-06-03 16:02:57 +0200

    Header, Navbar and Portofolio renderinf

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
 import React from 'react';
 import Header from './componets/header/Header'; 
-import About from './componets/about/About'
-import Contact from './componets/contact/Contact'
+import About from './componets/about/About';
+import Contact from './componets/contact/Contact';
+import Portofolio from './componets/portofolio/Portofolio';
+import Footer from './componets/footer/Footer';
+import Navbar from './componets/navbar/Navbar';
 
 function App() {
   return (
     <>
+      <Navbar />
       <Header />
       <About />
+      <Portofolio />
       <Contact />
-
+      <Footer />
     </>
   )
 }
