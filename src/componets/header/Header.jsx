6fed58ac9b9a1631898b2bef8a639273c905868c
Tomas Garcia <tomasgarciadev@gmail.com>
2022-06-04 11:54:16 +0200

    h elements added to Header component

--- src/componets/header/Header.jsx
+++ src/componets/header/Header.jsx
@@ -3,7 +3,13 @@ import './header.css';
 
 function Header() {
   return (
-    <div>Header</div>
+    <header>
+      <div className='container header__container'>
+        <h5>Hello, I am</h5>
+        <h1>Tomás García</h1>
+        <h5 className='text-light'>Fullstack Developer</h5>
+      </div>
+    </header>
   )
 }
 
