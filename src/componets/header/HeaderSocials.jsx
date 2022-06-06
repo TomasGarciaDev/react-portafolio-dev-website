24cfe7994bbd408afe6f8e9787f0a57df33ca359
Tomas Garcia <tomasgarciadev@gmail.com>
2022-06-06 21:37:31 +0200

    HeaderSocials components created

---
+++ src/componets/header/HeaderSocials.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const HeaderSocials = () => {
+  return (
+    <div className='header__socials'>
+      <a href='https://www.linkedin.com/in/tomas-garcia-64b145102/' target="_blank"></a>
+      <a href='https://github.com/TomasGarciaDev' target="_blank"></a>
+    </div>
+  )
+}
+
+export default HeaderSocials
