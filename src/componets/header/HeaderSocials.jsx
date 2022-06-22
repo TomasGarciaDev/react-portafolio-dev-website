d4ace28625fff37c67f17fc69a5ad86d9e67ebad
Tomas Garcia <tomasgarciadev@gmail.com>
2022-06-22 15:33:18 +0200

    Github icon changed

--- src/componets/header/HeaderSocials.jsx
+++ src/componets/header/HeaderSocials.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import {BsLinkedin} from 'react-icons/bs';
-import {FaGithubAlt} from 'react-icons/fa';
+import {FaGithub} from 'react-icons/fa';
 
 const HeaderSocials = () => {
   return (
     <div className='header__socials'>
       <a href='https://www.linkedin.com/in/tomas-garcia-64b145102/' target="_blank"><BsLinkedin/></a>
-      <a href='https://github.com/TomasGarciaDev' target="_blank"><FaGithubAlt /></a>
+      <a href='https://github.com/TomasGarciaDev' target="_blank"><FaGithub /></a>
     </div>
   )
 }
