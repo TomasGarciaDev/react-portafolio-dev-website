cf741089416654fc25e3b9e8551c2240a041aa97
Tomas Garcia <tomasgarciadev@gmail.com>
2022-06-26 13:12:06 +0200

    CSS for about__icon class

--- src/componets/about/About.jsx
+++ src/componets/about/About.jsx
@@ -24,17 +24,17 @@ function About() {
           </article>
         </div>
 
-        <div className="about__content">
+        {/* <div className="about__content">
           <article className="about__card">
             <BiBook className='about__icon' />
             <h5>Portfolio</h5>
             <small>10+ completed</small>
           </article>
-        </div>
+        </div> */}
 
-        <p>I am an enthusiastic and proactive Full-Stack developer and Musician. I love teamwork synergies, and I work hard to get a good job done while building a fun and healthy working environment. I use my experience as a musician to feed my work as a Developer, skills I consistently apply in teamwork settings.</p>
-        <p>I trained as a coder at Makers Academy, Europe's #1 Developer Bootcamp. I started as an eager amateur coder; this led me to win a Full Scholarship at Makers Academy. These intensive 16 weeks of Full-Stack training helped me grow into a competent and confident Developer.</p>
-        <p>I highly enjoy and love the continuous growth aspect of a tech career. My main goal is to grow my knowledge as a Front-End and Full-Stack Developer while helping to make my clients and companies thrive.</p>
+        <p>I am an enthusiastic and proactive Full-Stack developer and Musician. I love teamwork synergies, and I work hard to get a good job done while building a fun and healthy working environment. I use my experience as a musician to feed my work as a Developer, skills I consistently apply in teamwork settings.
+        I trained as a coder at Makers Academy, Europe's #1 Developer Bootcamp. I started as an eager amateur coder; this led me to win a Full Scholarship at Makers Academy. These intensive 16 weeks of Full-Stack training helped me grow into a competent and confident Developer.
+        I highly enjoy and love the continuous growth aspect of a tech career. My main goal is to grow my knowledge as a Front-End and Full-Stack Developer while helping to make my clients and companies thrive.</p>
 
         <a href="#contact" className="btn btn-primary">Contact</a>
       </div>
